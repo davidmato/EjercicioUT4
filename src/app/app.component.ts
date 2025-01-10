c13f1e6985f8b03b028ecd90fd4949d335b1d0de
davidmato <dmatodelalozapalomo@safareyes.es>
2025-01-10 17:12:28 +0100

    actualizacion de nuevo componente creado e implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CatalogoPeliculasComponent} from './catalogo-peliculas/catalogo-peliculas.component';
-import {GestionpedidosComponent} from './gestionpedidos/gestionpedidos.component';
+import {GestionpedidosComponent} from './gestion-pedidos/gestionpedidos.component';
 import {DatosMeteorologicosComponent} from './datos-meteorologicos/datos-meteorologicos.component';
 import {BibliotecaPalabrasComponent} from './biblioteca-palabras/biblioteca-palabras.component';
 import {BancoClientesComponent} from './banco-clientes/banco-clientes.component';
@@ -9,10 +9,11 @@ import {ZoologicoVirtualComponent} from './zoologico-virtual/zoologico-virtual.c
 import {
   TransformacionListadoProductosComponent
 } from './transformacion-listado-productos/transformacion-listado-productos.component';
+import {ClasificacionPalabrasComponent} from './clasificacion-palabras/clasificacion-palabras.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CatalogoPeliculasComponent, GestionpedidosComponent, DatosMeteorologicosComponent, BibliotecaPalabrasComponent, BancoClientesComponent, ZoologicoVirtualComponent, TransformacionListadoProductosComponent],
+  imports: [CatalogoPeliculasComponent, GestionpedidosComponent, DatosMeteorologicosComponent, BibliotecaPalabrasComponent, BancoClientesComponent, ZoologicoVirtualComponent, TransformacionListadoProductosComponent, ClasificacionPalabrasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
